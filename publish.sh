4bd3bef94a90b0777ef4cfc0c9b80bcc9bf65070
ebenpack <ebenpackwood@gmail.com>
2024-05-19 13:43:33 -0400

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+trunk build --release
+mkdir -p _publish_dir
+git -C _publish_dir/ init
+git -C _publish_dir/ config remote.origin.url >&- || git -C _publish_dir/ remote add origin $(git ls-remote --get-url origin)
+git -C _publish_dir/ fetch
+git -C _publish_dir/ checkout deploy || git -C _publish_dir/ checkout --orphan deploy
+# Delete everything (except dotfiles, e.g. .git), so we don't end up w/ cruft hanging around
+rm -rf _publish_dir/*
+cp -Rn dist/ _publish_dir/
+git -C _publish_dir/ add .
+git -C _publish_dir/ commit -m "Publish - $(date "+%Y-%m-%d %H:%M:%S")"
+git -C _publish_dir/ push --set-upstream origin deploy
+rm -rf _publish_dir/
